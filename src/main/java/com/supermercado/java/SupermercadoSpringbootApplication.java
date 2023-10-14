3a4ed46f65be6c8bded43f78032feefb114fb240
matheuspmbarcellos <matbarcellos21@gmail.com>
2023-10-14 15:07:08 -0300

    first commit

---
+++ src/main/java/com/supermercado/java/SupermercadoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.supermercado.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercadoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermercadoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supermercado/java/SupermercadoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermercado.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
